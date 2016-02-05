e19a53bc6c9e7201f2c8b255c9d5b3a122d36c53
David-Wobrock <david.wobrock@gmail.com>
2016-02-06 00:34:56 +0100

    alors comme ça on met plein de parenthèses en python?

--- input_parser.py
+++ input_parser.py
@@ -14,7 +14,6 @@ class Parser:
         self.max_col = 0
 
     def parse(self):
-        splitted_line = []
         self.size = 0
         with open(self.file_name, 'r') as f:
             for line in f:
@@ -25,30 +24,26 @@ class Parser:
 
     def get_column(self, c):
         col = []
-        if(c >= self.max_col):
+        if c >= self.max_col:
             return col
         for line in self.lines:
-            if(len(line) > c):
+            if len(line) > c:
                 col.append(line[c])
         return col
 
     def get_line(self, l):
-        return (self.lines[l] if l < self.size else [])
+        return self.lines[l] if l < self.size else []
 
     def get_columns(self, start, end):
-        while(start < end):
+        while start < end:
             yield(self.get_column(start))
             start += 1
 
     def get_lines(self, start, end):
-        while(start < end):
+        while start < end:
             yield(self.get_line(start))
             start += 1
 
     def get(self, l, c):
         line = self.get_line(l)
-        return (aformat(line[c]) if len(line) > c else None)
-
-
-
-
+        return aformat(line[c]) if len(line) > c else None
