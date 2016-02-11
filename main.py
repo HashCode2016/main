03d7b52ffd63909a805aaef282fa61149269bee0
David-Wobrock <david.wobrock@gmail.com>
2016-02-11 18:47:06 +0100

    add input files

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-from input_parser import Parser
+from hash_code_parser import HashCodeParser
 import sys
 import os
 from utils import INPUT_FILE_DIR
 
 if __name__ == "__main__":
-    input = sys.argv[1] if len(sys.argv) > 1 else 'test-input2016-logo.in'
+    input = sys.argv[1] if len(sys.argv) > 1 else 'file-de-bite.in'
     input = os.path.join(INPUT_FILE_DIR, input)
-    p = Parser(input)
+    p = HashCodeParser(input)
     p.parse()
-    print(p.get(0, 0))
-    print(p.get(0, 1))
-    print(p.get(0, 2))
-    print(p.get(1, 0))
-
-
+    p.coherence_test()
